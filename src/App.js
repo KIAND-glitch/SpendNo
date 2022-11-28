76d110ce5cf10cb9828d70ec1d0b84b4b41e0d0f
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-11-29 10:34:25 +1100

    init expo ts with prettier n eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -9,12 +18,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
