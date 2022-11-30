636a13591203b2846768b556db635f6d514dbc5e
seantanys <79890362+seantanys@users.noreply.github.com>
2022-11-30 21:19:29 +1100

    added a button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { Button } from '@rneui/themed';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,10 +16,13 @@ const styles = StyleSheet.create({
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Opend up</Text>
+        <Button title="CLICK ON MY TITS" />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
