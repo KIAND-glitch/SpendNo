d11c8516832b281db6ada46f3db3a720418afeb4
nboclzz <68681955+nboclzz@users.noreply.github.com>
2022-11-30 20:59:13 +1100

    add rneui and react navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,7 +14,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up</Text>
       <StatusBar style="auto" />
     </View>
   );
