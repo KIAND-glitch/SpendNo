454554dd67230de5c08c264ed7e302ccd7924f3d
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-12-01 21:35:14 +1100

    add basic profile screen

--- src/App.js
+++ src/App.js
@@ -1,53 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
-import { Button } from '@rneui/themed';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import Goals from './components/Goals';
 import Tabs from './navigation/Tabs';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
-
 const Tab = createBottomTabNavigator();
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Your Saving Goals"
-        onPress={() => navigation.navigate('Detailsr')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
-    </View>
-  );
-}
 
 function App() {
   return (
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,22 @@
+import { Avatar, Badge } from "@rneui/base";
+import { Text } from '@rneui/themed';
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 
 export default function Profile() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
+    <View style={{ flex: 1, top: 25, alignItems: 'center', paddingBottom: 10 }}>
+      <Avatar
+        size={140}
+        rounded
+        source={{ uri: "https://randomuser.me/api/portraits/women/31.jpg" }}
+      />
+      <Badge
+        status="success"
+        value=' $$$ '
+      />
+      <Text h4 style = {{padding: 10 }}>Monke Saves</Text>
+      <Text style = {{padding: 5 }}>monkesaves@spendno.com</Text>
     </View>
   );
 }
