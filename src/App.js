c310700f0f6f4367e2fc8d64098c23db3f4cb610
seantanys <79890362+seantanys@users.noreply.github.com>
2022-12-03 22:05:13 +1100

    added landing screen styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Screens from './navigation/Screens';
+import { Platform } from 'react-native';
+import { Button, lightColors, createTheme, ThemeProvider } from '@rneui/themed';
 
-const Tab = createBottomTabNavigator();
+const theme = createTheme({
+  lightColors: {
+    ...Platform.select({
+      default: lightColors.platform.android,
+      ios: lightColors.platform.ios,
+    }),
+  },
+});
 
 function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Screens />
-      </NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Screens />
+        </NavigationContainer>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,15 +1,90 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { ButtonGroup } from '@rneui/themed';
+import React, { useRef, useEffect } from 'react';
+import LottieView from 'lottie-react-native';
+import { View, StyleSheet } from 'react-native';
+import { Button, Text } from '@rneui/themed';
+import landingLottie from './landing-lottie.json';
 
 export default function Landing() {
+  const animation = useRef(null);
+  useEffect(() => {
+    // You can control the ref programmatically, rather than using autoPlay
+    // animation.current?.play();
+  }, []);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome To SpendNo</Text>
-      <ButtonGroup
-        buttons={['Sign Up', 'Log In']}
-        containerStyle={{ marginBottom: 20 }}
-      />
+    <View style={styles.appContainer}>
+      <View>
+        <Text h1 style={{ padding: 10 }}>
+          Spend
+          <Text h1 style={styles.logoNoText}>
+            No
+          </Text>
+        </Text>
+      </View>
+      <View>
+        <LottieView
+          autoPlay
+          source={landingLottie}
+          ref={animation}
+          style={styles.lottie}
+        />
+      </View>
+      <View>
+        <Text h3 style={{ paddingLeft: 10 }}>
+          Save Money
+        </Text>
+        <Text h3 style={{ paddingLeft: 10 }}>
+          Track Money
+        </Text>
+        <View style={styles.buttonGroup}>
+          <Button
+            title="Sign In"
+            buttonStyle={styles.button}
+            containerStyle={styles.buttonContainer}
+            titleStyle={{ fontWeight: 'bold' }}
+          />
+          <Button
+            title="Sign Up"
+            buttonStyle={styles.button}
+            containerStyle={styles.buttonContainer}
+            titleStyle={{ fontWeight: 'bold' }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logoText: {
+    fontWeight: '700',
+  },
+  logoNoText: {
+    color: '#1d3557',
+  },
+  appContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    padding: 10,
+  },
+  buttonGroup: {
+    padding: 10,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: 'black',
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 15,
+  },
+  buttonContainer: {
+    width: 150,
+    marginVertical: 10,
+  },
+  lottie: {
+    marginLeft: 20,
+    width: 500,
+    height: 450,
+  },
+});
